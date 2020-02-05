d3b5606af9d35c9f52aa88898d975606e9a01def
Matthew Robinson <matt_robinson1@yahoo.com>
2020-02-05 13:02:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import { component } 
+import { createStore }  from 'redux'
 
 import manageUsers from './reducers/manageUsers'
 
